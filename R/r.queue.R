26c466fa438aaa37f778b340811aede45fe4f36c
Gustavo H. Esteves <gesteves@gmail.com>
2020-07-09 17:59:42 -0300

    Add resample additional function to avoid problem into sample inside for

--- R/r.queue.R
+++ R/r.queue.R
@@ -8,17 +8,16 @@
 #' @param mu average rate that represents an exponential distribution for the times of services.
 #' @return will return values in the form of a matrix of four columns, with the arrival times, the times when the services were started, the departures and the number of the counter that was attended. 
 #' @author Mayara Almeida
-#' @example set.seed(10)
+#' @examples
+#' set.seed(10)
 #' data <- r.queue(10,2,1,4)
 #' @importFrom stats rexp
 #' @export
 
 
-#set.seed(10)
-
 r.queue<- function(n,c,lambda,mu){
 
-  ## Criando um vetor para guardar os tempos de sa?da de cada guich?
+  ## Criando um vetor para guardar os tempos de saida de cada guiche
   saida.guiches = vector("numeric", c)
   
   ## Simulando n tempos entre chegadas sucessivas com taxa lambda
@@ -29,21 +28,25 @@ r.queue<- function(n,c,lambda,mu){
   time.service <- rexp(n,1/mu)
   
   ## Criando vetores para armazenar o seguinte:
-  saida <- NULL ## Tempo de sa?da
-  tcs <- NULL ## Tempo do come?o do servi?o
+  saida <- NULL ## Tempo de saida
+  tcs <- NULL ## Tempo do comeco do servico
   guiche <- NULL ## Guiche de atendimento
+  
+  ## Criando uma funcao auxiliar, para evitar o problema de 
+  ## usar o sample diretamente...
+  resample <- function(x, ...) x[sample.int(length(x), ...)]
   
   for(i in 1:n) {
     idx = which(saida.guiches < tdc[i]) # Verifica se existem guiches livres
     if(length(idx) > 0) { # Caso exista
-      atendente <- sample(idx, 1) # Sorteia um entre os livres
-      tcs[i] <- tdc[i] # Inicio do servi?o coincide com a chegada
+      atendente <- resample(idx, 1) # Sorteia um entre os livres
+      tcs[i] <- tdc[i] # Inicio do servico coincide com a chegada
       guiche[i] = atendente
       saida[i] <- saida.guiches[atendente] <- tcs[i] + time.service[i]
     }
     else { # Caso todos estejam ocupados
       atendente <- which.min(saida.guiches) # Pega o primeiro a desocupar
-      tcs[i] <- saida.guiches[atendente] # Inicio do servi?o qdo desocupar o 1.
+      tcs[i] <- saida.guiches[atendente] # Inicio do servico qdo desocupar o 1.
       guiche[i] = atendente
       saida[i] <- saida.guiches[atendente] <- tcs[i] + time.service[i]
       
@@ -54,6 +57,3 @@ r.queue<- function(n,c,lambda,mu){
   return(cbind(tdc, tcs, saida, guiche))
   
 }
-
-
-#data <- r.queue(10,1,1,4)
